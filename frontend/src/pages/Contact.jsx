7b7108cfb9097b939cee90a64cde912e29280b04
Muhammad Alam <malam53345@gmail.com>
2023-09-02 15:26:41 +0500

    login and signup pages completed

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,30 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className="heading text-center">Contact Us</h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text__para">
+          Get a technical issue? Want to send feedback about a beta feature? Let
+          us know.
+        </p>
+
+        <form action="#" className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form__label">
+              Your Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@gmail.com"
+              className="form__input mt-1"
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
