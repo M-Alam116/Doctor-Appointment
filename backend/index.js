b6041cee8b208d9059039bf02116d6d336a978eb
Muhammad Alam <malam53345@gmail.com>
2023-09-08 22:18:30 +0500

    signup and login api completed

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/user.js";
 
 dotenv.config();
 
@@ -34,6 +36,8 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
 
 app.listen(port, () => {
   connectDB();
